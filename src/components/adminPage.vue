04da03aedd583e65ff93e00053446d225253cc87
sadfroggy-max <xjz17317307810@163.com>
2024-07-29 16:54:15 +0800

    完成了最终布局

--- src/components/adminPage.vue
+++ src/components/adminPage.vue
@@ -8,65 +8,82 @@
       <ul>
         <li>
           <RouterLink to="#">
-            <div class="sidebutton">
+            <div class="sidebutton" @click="toggleHomeInfo">
               <img src="@/assets/pics/home.png" alt="智慧首页" width="34" height="30" style="margin: 0 0 0 2px ">
               <p>智慧首页</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isHomeInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
           <RouterLink to="#">
-            <div class="sidebutton">
+            <div class="sidebutton" @click="toggleHRInfo">
               <img src="@/assets/pics/hr_manage.png" alt="人事管理" width="20" height="20" style="margin: 0 8px 0 ">
               <p>人事管理</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isHRInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
           <RouterLink :to="{ path: '/customer_infoPage' }">
-            <div class="sidebutton">
+            <div class="sidebutton" @click="toggleCustomerInfo">
               <img src="@/assets/pics/cus_info.png" alt="顾客信息" width="20" height="20" style="margin: 0 8px 0 ">
               <p>顾客信息</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isCustomerInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
-          <RouterLink to="#">
-            <div class="sidebutton">
+          <RouterLink :to="{ path: '/pill_managePage' }">
+            <div class="sidebutton" @click="togglePillInfo">
               <img src="@/assets/pics/pill_manage.png" alt="医药管理" width="20" height="20" style="margin: 0 8px 0 ">
               <p>医药管理</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isPillInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
           <RouterLink to="#">
-            <div class="sidebutton">
+            <div class="sidebutton" @click="toggleMoneyInfo">
               <img src="@/assets/pics/money_manage.png" alt="收支管理" width="20" height="20" style="margin: 0 8px 0 ">
               <p>收支管理</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isMoneyInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
           <RouterLink to="#">
-            <div class="sidebutton">
+            <div class="sidebutton" @click="toggleServiceInfo">
               <img src="@/assets/pics/service_manage.png" alt="服务管理" width="20" height="20" style="margin: 0 8px 0 ">
               <p>服务管理</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isServiceInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
           <RouterLink to="#">
-            <div class="sidebutton">
-              <img src="@/assets/pics/hospital_manage.png" alt="分院管理" width="25" height="25" style="margin: 0 2px 0 8px ">
+            <div class="sidebutton" @click="toggleHospitalInfo">
+              <img src="@/assets/pics/hospital_manage.png" alt="分院管理" width="25" height="25"
+                style="margin: 0 2px 0 8px ">
               <p>分院管理</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isHospitalInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
         <li>
           <RouterLink to="#">
-            <div class="sidebutton">
+            <div class="sidebutton" @click="toggleORInfo">
               <img src="@/assets/pics/or_manage.png" alt="手术室管理" width="25" height="25" style="margin: 0 2px 0 8px ">
               <p>手术室管理</p>
+              <img src="@/assets/pics/arrow-down.png" :class="{ 'arrow-icon-rotate': isORInfoVisible }" alt="箭头"
+                class="arrow-icon">
             </div>
           </RouterLink>
         </li>
@@ -77,6 +94,105 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isHomeInfoVisible: false,
+      isHRInfoVisible: false,
+      isCustomerInfoVisible: false,
+      isPillInfoVisible: false,
+      isMoneyInfoVisible: false,
+      isServiceInfoVisible: false,
+      isHospitalInfoVisible: false,
+      isORInfoVisible: false,
+    };
+  },
+  methods: {
+    toggleHomeInfo() {
+      this.isHomeInfoVisible = true;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = false;
+    },
+    toggleHRInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = true;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = false;
+    },
+    toggleCustomerInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = true;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = false;
+    },
+    togglePillInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = true;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = false;
+    },
+    toggleMoneyInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = true;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = false;
+    },
+    toggleServiceInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = true;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = false;
+    },
+    toggleHospitalInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = true;
+      this.isORInfoVisible = false;
+    },
+    toggleORInfo() {
+      this.isHomeInfoVisible = false;
+      this.isHRInfoVisible = false;
+      this.isCustomerInfoVisible = false;
+      this.isPillInfoVisible = false;
+      this.isMoneyInfoVisible = false;
+      this.isServiceInfoVisible = false;
+      this.isHospitalInfoVisible = false;
+      this.isORInfoVisible = true;
+    }
+  }
+}
+</script>
 
 <style>
 * {
@@ -148,5 +264,16 @@ body {
   text-align: center;
   padding-left: 40px;
   align-items: center;
+}
+
+.arrow-icon {
+  width: 20px;
+  height: 20px;
+  margin-left: 8px;
+  transition: transform 0.3s;
+}
+
+.arrow-icon-rotate {
+  transform: rotate(-90deg);
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import adminPage from "../components/adminPage.vue"
 import customer_infoPage from "../views/customer_infoPage.vue"
+import pill_managePage from "../views/pill_managePage.vue"
 //创建路由器
 const router = createRouter({
     history: createWebHistory(),
@@ -12,7 +13,12 @@ const router = createRouter({
                 {
                     path: "customer_infoPage",
                     component: customer_infoPage,
-                }]
+                },
+                {
+                    path: "pill_managePage",
+                    component: pill_managePage,
+                },
+            ]
         }
     ]
 })
