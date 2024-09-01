acab6b518281e39298681934bed0f17fd5400a04
bapbary <914489891@qq.com>
2024-09-01 16:19:37 +0800

    1
    1

--- AestheticsLink/WebAPI/Controllers/OrderPlaceController.cs
+++ AestheticsLink/WebAPI/Controllers/OrderPlaceController.cs
@@ -46,12 +46,20 @@ namespace WebAPI.Controllers
                             couID = order.COU_ID,
                         });
                     }
+                    //会员打折
+                    var viplevel = DbContext.db.Ado.SqlQuerySingle<MEMBER>(
+                    "SELECT * FROM CUSTOMER NATURAL JOIN MEMBER WHERE CUS_ID = :id ",
+                    new
+                    {
+                        id = placeOrderDto.clientid,
+                    });
+                    decimal pay = order.PAID_AMOUNT * viplevel.DISCOUNT;
                     //减去付款金额
                     DbContext.db.Ado.ExecuteCommand(
                         "UPDATE CUSTOMER SET BALANCE = BALANCE - :balance WHERE CUS_ID = :cusID",
                         new
                         {
-                            balance = order.PAID_AMOUNT,
+                            balance = pay,
                             cusID = order.CUS_ID,
                         });
                     //事物提交
---
+++ AestheticsLink/WebModel/Entity/MEMBER.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebModel.Entity
+{
+    public class MEMBER
+    {
+        public string VIPLEVEL { get; set; }
+        public decimal DISCOUNT { get; set; }
+    }
+}
